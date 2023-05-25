241e630e6ad824fac14ac88c05b771528978739c
nooburger81 <brandonsebastian@gmail.com>
2023-05-25 17:49:23 -0400

    added script files

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const path = require('path');
 require('dotenv').config();
 const express = require('express');
 const sequelize = require('./config/connection');
+const routes = require('./controllers/');
+const exphbs = require('express-handlebars')
+
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
