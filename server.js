328762368a641c9bb158f889ee75e85a024b4281
nooburger81 <brandonsebastian@gmail.com>
2023-05-25 17:31:52 -0400

    database and models created

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const path = require('path');
 require('dotenv').config();
 const express = require('express');
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(process.env.PORT ||3001, () => console.log('Now listening'));
