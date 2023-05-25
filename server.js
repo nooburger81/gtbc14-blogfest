183810ea370d2e7aedf065eff397773c97f7f9b4
nooburger81 <brandonsebastian@gmail.com>
2023-05-25 17:53:38 -0400

    set up sessions storage

--- server.js
+++ server.js
@@ -5,12 +5,24 @@ const sequelize = require('./config/connection');
 const routes = require('./controllers/');
 const exphbs = require('express-handlebars')
 const helpers = require('./utils/helpers');
+const session = require('express-session')
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 
 const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: { maxAge: 7200000 },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
